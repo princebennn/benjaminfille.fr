f58d6057511731a5fdfc902d3e92679df160cab9
princeben <benjamin.fille@epitech.eu>
2017-06-07 13:37:43 -0400

    add social and technos

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Just for you I rotate the background on smartphone ;)
+
 var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 if (width < height)
